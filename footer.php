a34282afde9991973c39dafd26ae769e5381b0ab
raghav <raghu99931036@gmail.com>
2025-01-16 07:31:36 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,30 @@
+
+        <!-- start wpo-site-footer -->
+        <footer class="wpo-site-footer">
+            <div class="lower-footer">
+                <div class="container">
+                    <div class="row">
+                        <!-- <div class="separator"></div> -->
+                        <p class="copyright">@ 2025 developed By Raghvendra. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end wpo-site-footer -->
+    </div>
+    <!-- end of page-wrapper -->
+
+    <!-- All JavaScript files
+    ================================================== -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!-- Plugins for this template -->
+    <script src="assets/js/modernizr.custom.js"></script>
+    <script src="assets/js/jquery.dlmenu.js"></script>
+    <script src="assets/js/jquery-plugin-collection.js"></script>
+    <!-- Moving Animation -->
+    <script src="assets/js/moving-animation.js"></script>
+    <!-- Custom script for this template -->
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
